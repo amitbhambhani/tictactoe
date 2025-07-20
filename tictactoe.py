51658312b2b2211bf2af51d8ddf0c54a405b436f
Amit Bhambhani <ambh.dev@gmail.com>
2025-07-20 01:41:27 -0400

    Implemented board; digits for selection are greyed out

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,36 @@
+from termcolor import colored
+
+layout = {
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+}
+
+
 def board(layout):
-    pass
+    print("+---+---+---+")
+    print(
+        f"| {colored(layout[1], "grey")} | {colored(layout[2], "grey")} | {colored(layout[3], "grey")} |"
+    )
+    print("+---+---+---+")
+    print(
+        f"| {colored(layout[4], "grey")} | {colored(layout[5], "grey")} | {colored(layout[6], "grey")} |"
+    )
+    print("+---+---+---+")
+    print(
+        f"| {colored(layout[7], "grey")} | {colored(layout[8], "grey")} | {colored(layout[9], "grey")} |"
+    )
+    print("+---+---+---+")
+    return None
+
+
+board(layout)
 
 
 def choice(choice, layout, turn):
