ef882e3f36e093ca3d72ae783027a1fbcb0751f3
Amit Bhambhani <ambh.dev@gmail.com>
2025-07-20 02:17:27 -0400

    Added game logic and colored X/O. Implemented placeMark. Need to add safeguard such that selections of the same cell are rejected, and the user is prompted to make another choice --- currently leads to infinite loop.

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import os
+
 from termcolor import colored
 
 layout = {
@@ -32,8 +34,11 @@ def board(layout):
 
 def choice(choice, layout, turn):
     num = int(choice)
-    if num > 0 and num < 10:
-        layout[num] = turn
+    if num > 0 and num < 10 and layout[num] == str(num):
+        if turn == "X":
+            layout[num] = colored("X", (230, 69, 83))
+        elif turn == "O":
+            layout[num] = colored("O", (4, 165, 229))
         return True
     else:
         print("Invalid choice, try again")
@@ -44,9 +49,9 @@ def checkWin(layout, patterns):
     xPattern = ""
     oPattern = ""
     for key in layout:
-        if layout[key] == "X":
+        if layout[key] == colored("X", (230, 69, 83)):
             xPattern += f"{key}"
-        if layout[key] == "O":
+        if layout[key] == colored("O", (4, 165, 229)):
             oPattern += f"{key}"
     if xPattern in patterns or oPattern in patterns:
         return True
@@ -54,8 +59,55 @@ def checkWin(layout, patterns):
 
 
 def placeMark(layout, patterns, turn):
-    pass
+    if turn % 2 == 0:
+        selection = input("Place an X!\n")
+        correctChosen = False
+        while correctChosen == False:
+            correctChosen = choice(selection, layout, "X")
+            if correctChosen == True and checkWin(layout, patterns) == True:
+                os.system("clear")
+                board(layout)
+                print("X wins!")
+                return True
+    else:
+        selection = input("Place an O!\n")
+        correctChosen = False
+        while correctChosen == False:
+            correctChosen = choice(selection, layout, "O")
+            if correctChosen == True and checkWin(layout, patterns) == True:
+                os.system("clear")
+                board(layout)
+                print("O wins!")
+                return True
 
 
 def game():
-    pass
+    turn = 0
+
+    patterns = {"123", "456", "789", "159", "258", "357", "147", "369"}
+
+    layout = {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+    }
+
+    while True:
+        board(layout)
+        if turn == 9:
+            print("Draw")
+            break
+        win = placeMark(layout, patterns, turn)
+        if win == True:
+            break
+        turn += 1
+        os.system("clear")
+
+
+game()
